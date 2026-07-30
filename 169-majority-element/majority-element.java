01469cc3c552647959eeed76ea2c4c224fba12e0
Shreyash Chandra <hishreyashgiis@gmail.com>
2026-07-30 23:08:24 +0530

    Time: 7 ms (45.93%) | Memory: 55.6 MB (59.80%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,10 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        // Sort the array first
         Arrays.sort(nums);
-        
-        // The majority element will always be at the middle index
         return nums[nums.length / 2];
     }
 }
