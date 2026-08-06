b2ecd1481df061145f1c52e4015e60fb947b9a01
Shreyash Chandra <hishreyashgiis@gmail.com>
2026-08-06 13:28:52 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (50.26%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while (digiMul(n) % t != 0) {
+            n++;
+        }
+        return n;
+    }
+    private int digiMul(int num) {
+        int prod = 1;
+        while (num > 0) {
+            int digit = num % 10;
+            prod *= digit;
+            num /= 10;
+        }
+        return prod;
+    }
+}
